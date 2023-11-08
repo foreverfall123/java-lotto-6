8b0bafa227d3aacda68e1e960e5ef6fc36dbf00d
foreverfall123 <foreverfall@naver.com>
2023-11-08 20:11:02 +0900

    Feature create Calc Class
    - create calc rate of return function

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,6 +1,13 @@
 package lotto;
 
 public class Application {
+
+    public static final String PrizePlease = "구입 금액을 입력해 주세요.";
+    public static final String BuyCheck = "개를 구매했습니다.";
+    public static final String LottoNumberPlease = "당첨 번호를 입력해 주세요.";
+    public static final String BonusNumberPlease = "보너스 번호를 입력해 주세요.";
+
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
---
+++ src/main/java/lotto/Calc.java
@@ -0,0 +1,25 @@
+package lotto;
+
+public class Calc {
+
+    public static final int LottoAmount = 1000;
+
+    private int buyAmount = 0;
+    private int benefit = 0;
+
+    public Calc(int buyAmount){
+        this.buyAmount = buyAmount;
+    }
+
+    public int getLottoCount(){
+        return buyAmount / LottoAmount;
+    }
+
+    public void setBenefit(int benefit){
+        this.benefit = benefit;
+    }
+
+    public double getRateOfReturn(){
+        return (((benefit - buyAmount) / buyAmount * 100.0) * 10.0) / 10.0;
+    }
+}
